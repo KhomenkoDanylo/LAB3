ef47f914ed67f3e4122d5072bf92c20fe2badd66
KhomenkoDanylo <danylohomenko131@gmail.com>
2022-10-08 09:36:55 +0300

    LAB3

---
+++ LAB3/1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+public class _1
+    {
+    public static void Main()
+        {
+        string a = Console.ReadLine();
+        string b = Console.ReadLine();
+        Debug.WriteLine(a);
+        Debug.WriteLine(b);
+        Console.WriteLine(a);
+        }
+    }
